fc126f3eb8e899ec2772f9881612d91f6909a53d
Kaidey <zezepinto@hotmail.com>
2020-02-12 17:25:24 +0000

    v4.0.0; Fix #9

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
 server.listen(process.env.PORT || 5000, () => {
     console.log(`Server running`);
 
-    bot.sendMessage(chat_id = process.env.CHAT_ID, text = 'Updated to version 3.0.0\nAdded:\n- Message on mention\n- /next command to give how much time is left to the next 4:20 or to the next 4:20 in a specific city\nChanged:\n- Refactoring of the code in multiple files\n- Added Riga as a main city in UTC+2\n- Message of UTC+2\n\nIf you have any doubt, contribution or suggestion please feel free to hit me up in Telegram @suembra or create an issue in Github https://github.com/demo-hub/PinkGlobal420TelegramBot')
+    //bot.sendMessage(chat_id = process.env.CHAT_ID, text = 'Updated to version 3.0.0\nAdded:\n- Message on mention\n- /next command to give how much time is left to the next 4:20 or to the next 4:20 in a specific city\nChanged:\n- Refactoring of the code in multiple files\n- Added Riga as a main city in UTC+2\n- Message of UTC+2\n\nIf you have any doubt, contribution or suggestion please feel free to hit me up in Telegram @suembra or create an issue in Github https://github.com/demo-hub/PinkGlobal420TelegramBot')
 
     notification.timeNotification(bot);
 
@@ -26,5 +26,7 @@ server.listen(process.env.PORT || 5000, () => {
     commands.mentions(bot)
 
     commands.next(bot)
+
+    commands.ping(bot)
 
 });
